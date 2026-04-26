8da7a88f51f288174762aaaae18f8c433152c66b
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-04-26 20:57:34 +0530

    In Heapify - written build maxHeap function using Array

--- Heaps/Heapify.java
+++ Heaps/Heapify.java
@@ -1,7 +1,7 @@
 
 class Heapify {
 
-    public int[] buildHeap(int[] A) {
+    public int[] buildMinHeap(int[] A) {
         int n = A.length;
 
         for (int i = n / 2 - 1; i >= 0; i--) {
@@ -11,7 +11,17 @@ class Heapify {
         return A;
     }
 
-    // Build Heap using Array
+    public int[] buildMaxHeap(int[] A) {
+        int n = A.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            maxHeapify(A, n, i);
+        }
+
+        return A;
+    }
+
+    // Build Min-Heap using Array
     private void heapify(int[] A, int n, int i) {
         int smallest = i;
         int left = 2 * i + 1;
@@ -31,6 +41,29 @@ class Heapify {
             A[smallest] = temp;
 
             heapify(A, n, smallest);
+        }
+    }
+
+    // Build Max-Heap using Array
+    private void maxHeapify(int[] A, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && A[left] > A[largest]) {
+            largest = left;
+        }
+
+        if (right < n && A[right] > A[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int temp = A[i];
+            A[i] = A[largest];
+            A[largest] = temp;
+
+            heapify(A, n, largest);
         }
     }
 }
