f2f6393d0b45c818255fe1e54147bd65915eb531
Vinayak Jadhav <vjadhav1010@gmail.com>
2026-05-12 23:48:37 +0530

    Minimum number of squares to reach the sum

---
+++ DP-01/MinNumberOfSquares.java
@@ -0,0 +1,23 @@
+
+public class MinNumberOfSquares {
+
+    public int countMinSquares(int A) {
+        int[] dp = new int[A + 1];
+
+        // Initialize with max value
+        for (int i = 1; i <= A; i++) {
+            dp[i] = i;
+        }
+
+        dp[0] = 0;
+
+        for (int i = 1; i <= A; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+
+        return dp[A];
+
+    }
+}
